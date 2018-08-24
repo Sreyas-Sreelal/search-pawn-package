4d37c17c04699e999a00e55be2381a100351664f
__SyS__ <sreyassreelal@gmail.com>
2018-08-24 14:56:46 +0530

    improved replacement with accurate position

--- src/extension.ts
+++ src/extension.ts
@@ -7,51 +7,50 @@ const dropRightWhile = require('lodash.droprightwhile');
 const range = require('lodash.range');
 
 type Package = {
-    user: string;
-    repo: string;
+	user: string;
+	repo: string;
 };
 var packages:Package[];
 
 const GetCompletionItem = (Range: vscode.Range) => (pack: Package) => {
-    const { user, repo } = pack;
-    const content = new vscode.CompletionItem(user+"/"+repo, vscode.CompletionItemKind.Text);
-    content.additionalTextEdits = [vscode.TextEdit.delete(Range)];
-    content.insertText = `"${user}/${repo}"`;
-    return content;
+	const { user, repo } = pack;
+	const content = new vscode.CompletionItem(user+"/"+repo, vscode.CompletionItemKind.Text);
+	content.additionalTextEdits = [vscode.TextEdit.delete(Range)];
+	content.insertText = `"${user}/${repo}"`;
+	return content;
 };
 
 async function GetPackagelist(){
-    
-    try {
-        let response =  await axios.get("https://list.packages.sampctl.com");
-        packages = response.data;
-    }catch(err){
-        console.error(err);
-        vscode.window.showErrorMessage("Couldn't connect to list.packages.sampctl.com");
-    }
+	try {
+		let response =  await axios.get("https://list.packages.sampctl.com");
+		packages = response.data;
+	} catch(err) {
+		console.error(err);
+		vscode.window.showErrorMessage("Couldn't connect to list.packages.sampctl.com");
+	}
 
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    //vscode.window.showInformationMessage('Activated search pawn package!');
-    console.log('search-pawn-package is activated');
-    GetPackagelist();
+	//vscode.window.showInformationMessage('Activated search pawn package!');
+	console.log('search-pawn-package is activated');
+	GetPackagelist();
 
-    vscode.languages.registerCompletionItemProvider({ language: 'json', pattern: '**/pawn.json' }, {
-        async provideCompletionItems (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-            const postline = position.line;
-            const { text } = document.lineAt(postline);
-            const currentLineReplaceRange = new vscode.Range(new vscode.Position(postline, 0), new vscode.Position(postline, text.length));
-
-            if(packages !== undefined){
-                return packages.map(GetCompletionItem(currentLineReplaceRange));
-            }
-        },
-            resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken) {
-                return item;
-            }
-            
-    });
+	vscode.languages.registerCompletionItemProvider({ language: 'json', pattern: '**/pawn.json' }, {
+		async provideCompletionItems (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			const editor = vscode.window.activeTextEditor;
+			const posline = editor.selection.active;
+			const { text } = document.lineAt(posline);
+			const currentLineReplaceRange = new vscode.Range(new vscode.Position(posline.line, position.character), new vscode.Position(posline.line, text.length));
+			if(packages !== undefined){
+				return packages.map(GetCompletionItem(currentLineReplaceRange));
+			}
+		},
+		resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken) {
+			return item;
+		}
+			
+	});
 
 }
 
