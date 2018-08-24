93620c050529fa828c7ddd3bc9abffedc2df06ca
Sreyas <Sreyassreelal@gmail.com>
2018-08-24 06:34:21 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# search-pawn-package
+
+This is a simple vscode extension which provides pawn package suggestions in your pawn.json with the help of SouthClaw' pawndex api.
+This extension is still under developement and not ready to use.Also this is the first time i'm working on an extension for vscode and i'm still a rookie in typescript therefore the code won't be perefect and i really appreciate reviews or pull requests so that i can improve :).
+
+## Usage
+![alt text](https://github.com/Sreyas-Sreelal/search-pawn-package/blob/master/images/usage.gif?raw=true "usage")
---
+++ src/extension.ts
@@ -0,0 +1,58 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import axios from 'axios';
+
+const dropRightWhile = require('lodash.droprightwhile');
+const range = require('lodash.range');
+
+type Package = {
+    user: string;
+    repo: string;
+};
+var packages:Package[];
+
+const GetCompletionItem = (Range: vscode.Range) => (pack: Package) => {
+    const { user, repo } = pack;
+    const content = new vscode.CompletionItem(user+"/"+repo, vscode.CompletionItemKind.Text);
+    content.additionalTextEdits = [vscode.TextEdit.delete(Range)];
+    content.insertText = `"${user}/${repo}"`;
+    return content;
+};
+
+async function GetPackagelist(){
+    
+    try {
+        let response =  await axios.get("https://list.packages.sampctl.com");
+        packages = response.data;
+    }catch(err){
+        console.error(err);
+        vscode.window.showErrorMessage("Couldn't connect to list.packages.sampctl.com");
+    }
+
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    //vscode.window.showInformationMessage('Activated search pawn package!');
+    console.log('search-pawn-package is activated');
+    GetPackagelist();
+
+    vscode.languages.registerCompletionItemProvider({ language: 'json', pattern: '**/pawn.json' }, {
+        async provideCompletionItems (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+            const postline = position.line;
+            const { text } = document.lineAt(postline);
+            const currentLineReplaceRange = new vscode.Range(new vscode.Position(postline, 0), new vscode.Position(postline, text.length));
+
+            if(packages !== undefined){
+                return packages.map(GetCompletionItem(currentLineReplaceRange));
+            }
+        },
+            resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken) {
+                return item;
+            }
+            
+    });
+
+}
+
+export function deactivate() {}
